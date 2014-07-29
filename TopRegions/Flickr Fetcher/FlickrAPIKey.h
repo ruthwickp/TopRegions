88b5777b327c1b32f50567ef54898d2b4ce16297
ruthwickp <ruthwickp@yahoo.com>
2014-07-29 10:19:25 -0700

    Added Flickr API files

---
+++ TopRegions/Flickr Fetcher/FlickrAPIKey.h
@@ -0,0 +1,11 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Fall 2013.
+//  Copyright 2013 Stanford University. All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+
+#define FlickrAPIKey @"069408c2e1031a0fa0356de1f5234832"
